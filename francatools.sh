6279c12c7311735c230d25a44df3d4705d3c4eb7
Therac-25 <darkskull2406@gmail.com>
2025-08-11 20:02:24 -0300

    Update francatools.sh

--- francatools.sh
+++ francatools.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# ===== Cores =====
+VERDE='\033[1;32m'
+VERMELHO='\033[1;31m'
+AZUL='\033[1;34m'
+AMARELO='\033[1;33m'
+ROXO='\033[1;35m'
+CYAN='\033[1;36m'
+RESET='\033[0m'
+
 clear
-echo "============================"
-echo "    🇫🇷 FRANÇA TOOLS v1.      "
-echo "============================"
+echo -e "${ROXO}============================"
+echo -e "    🇫🇷 ${CYAN}FRANÇA TOOLS v1${ROXO}      "
+echo -e "============================${RESET}"
 echo
-echo "[  1 ] - NMAP"
-echo "[  2 ] - NETCAT"
-echo "[  3 ] - SHERLOCK"
-echo "[  4 ] - TSHARK"
-echo "[  5 ] - OH-MY-ZSH [Config Terminal]"
-echo "[  6 ] - SPARTA"
-echo "[  7 ] - SOCIALFISH"
-echo "[  8 ] - SQLMAP"
-echo "[  9 ] - NIKTO"
-echo "[ 10 ] - NEOVIM"
-echo "[ 11 ] - DIRB"
-echo "[ 12 ] - ADMIN-FINDER"
-echo "[ 00 ] - HKBRS [CHAT/CANAL]"
-echo "[ 99 ] - MELHOR BOT DE CONSULTA"
-echo "[ 00 ] - SAIR"
-echo
+echo -e "${VERDE}[  1 ]${RESET} - ${AMARELO}NMAP${RESET}"
+echo -e "${VERDE}[  2 ]${RESET} - ${AMARELO}NETCAT${RESET}"
+echo -e "${VERDE}[  3 ]${RESET} - ${AMARELO}SHERLOCK${RESET}"
+echo -e "${VERDE}[  4 ]${RESET} - ${AMARELO}TSHARK${RESET}"
+echo -e "${VERDE}[  5 ]${RESET} - ${AMARELO}OH-MY-ZSH [Config Terminal]${RESET}"
+echo -e "${VERDE}[  6 ]${RESET} - ${AMARELO}SPARTA${RESET}"
+echo -e "${VERDE}[  7 ]${RESET} - ${AMARELO}SOCIALFISH${RESET}"
+echo -e "${VERDE}[  8 ]${RESET} - ${AMARELO}SQLMAP${RESET}"
+echo -e "${VERDE}[  9 ]${RESET} - ${AMARELO}NIKTO${RESET}"
+echo -e "${VERDE}[ 10 ]${RESET} - ${AMARELO}NEOVIM${RESET}"
+echo -e "${VERDE}[ 11 ]${RESET} - ${AMARELO}DIRB${RESET}"
+echo -e "${VERDE}[ 12 ]${RESET} - ${AMARELO}ADMIN-FINDER${RESET}"
+echo -e "${VERDE}[ 00 ]${RESET} - ${CYAN} THERAC NETWORK [CHAT/CANAL]${RESET}"
+echo -e "${VERDE}[ 99 ]${RESET} - ${CYAN}MELHOR BOT DE CONSULTA${RESET}"
+echo -e "${VERMELHO}[ 00 ]${RESET} - ${ROXO}SAIR${RESET}"
 
 read -p "Escolha uma opção: " opt
 
@@ -105,10 +113,10 @@ case $opt in
     echo "Para rodar: python3 admin-finder.py -u URL"
     ;;
  00)
-    echo "Canal HKBRS: https://t.me/hkbrs"
+    echo "Canal Telegram "em desenvolvimento": https://t.me/+y0EajX0UjC8zNGZh"
     ;;
  99)
-    echo "Melhor bot de consulta: https://t.me/botdeconsulta"
+    echo "Melhor bot de consulta: https://t.me/blackconsultaasbot"
     ;;
  *)
     echo "Saindo..."
